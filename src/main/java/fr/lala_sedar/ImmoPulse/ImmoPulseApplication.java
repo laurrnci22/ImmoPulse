fde47113d9b0027930d938640e020eba6e4d7c15
sedar007 <dev@adjysedar.fr>
2026-01-30 14:26:54 +0100

    init project

---
+++ src/main/java/fr/lala_sedar/ImmoPulse/ImmoPulseApplication.java
@@ -0,0 +1,13 @@
+package fr.lala_sedar.ImmoPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/lala_sedar/ImmoPulse/ImmoPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lala_sedar.ImmoPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmoPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
