5f69220f7c8af44edcf09d206644863444751903
LalaBritta <randrianalisoa.lbl@gmail.com>
2026-01-30 17:28:19 +0100

    init project + test controller + docker compose

---
+++ immopulse-api/src/main/java/fr/lala_sedar/ImmoPulse/controllers/TestController.java
@@ -0,0 +1,16 @@
+package fr.lala_sedar.ImmoPulse.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("")
+    public String index() {
+        return "Hello World!";
+    }
+}
