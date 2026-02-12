97f5304647cdf419f190c2eba1863a8fa7d89158
sedar007 <dev@adjysedar.fr>
2026-02-12 14:01:55 +0100

    update docker to use compress data

--- init-scripts/clickhouse/init.sql
+++ init-scripts/clickhouse/init.sql
@@ -17,7 +17,6 @@ CREATE TABLE IF NOT EXISTS dvf (
 ) ENGINE = MergeTree()
 ORDER BY (code_departement, commune, date_mutation);
 
-
 INSERT INTO dvf (
     date_mutation,
     nature_mutation,
@@ -35,8 +34,10 @@ INSERT INTO dvf (
     surface_terrain
 )
 SELECT
-    parseDateTimeBestEffortOrNull(c9) AS date_mutation,
+    -- Conversion explicite en Date
+    toDate(parseDateTimeBestEffortOrNull(c9)) AS date_mutation,
     c10 AS nature_mutation,
+    -- Gestion des virgules françaises
     toFloat64OrZero(replaceAll(c11, ',', '.')) AS valeur_fonciere,
     c12 AS adresse_numero,
     c14 AS type_de_voie,
@@ -46,13 +47,14 @@ SELECT
     c19 AS code_departement,
     c22 AS section,
     c23 AS no_plan,
-    toFloat32OrNull(replaceAll(c39, ',', '.')) AS surface_reelle_bati, -- Correction index (39)
-    c37 AS type_local, -- Correction index (37)
-    toFloat32OrNull(replaceAll(c43, ',', '.')) AS surface_terrain -- Correction index (43)
+    toFloat32OrNull(replaceAll(c39, ',', '.')) AS surface_reelle_bati,
+    c37 AS type_local,
+    toFloat32OrNull(replaceAll(c43, ',', '.')) AS surface_terrain
 FROM file(
-    'ValeursFoncieres-2025-S1.csv', 
+    -- CHEMIN INTERNE DOCKER + EXTENSION .GZ
+    '/var/lib/clickhouse/user_files/ValeursFoncieres-2025-S1.csv.gz', 
     'CSV', 
     'c1 String, c2 String, c3 String, c4 String, c5 String, c6 String, c7 String, c8 String, c9 String, c10 String, c11 String, c12 String, c13 String, c14 String, c15 String, c16 String, c17 String, c18 String, c19 String, c20 String, c21 String, c22 String, c23 String, c24 String, c25 String, c26 String, c27 String, c28 String, c29 String, c30 String, c31 String, c32 String, c33 String, c34 String, c35 String, c36 String, c37 String, c38 String, c39 String, c40 String, c41 String, c42 String, c43 String'
 )
-WHERE c9 != 'Date mutation'
-SETTINGS format_csv_delimiter = ';';
+WHERE c9 != 'Date mutation' -- On ignore la ligne d'en-tête
+SETTINGS format_csv_delimiter = ';'; -- Les fichiers DVF sont souvent en point-virgule
